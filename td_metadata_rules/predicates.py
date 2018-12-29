42f101f330ad3534a2cb1ea9b785a219e7a4f0c1
tumiemosweu <imosweuk@gmail.com>
2018-12-29 23:34:57 +0200

    add test for predicates

--- td_metadata_rules/predicates.py
+++ td_metadata_rules/predicates.py
@@ -3,37 +3,28 @@ from edc_metadata_rules import PredicateCollection
 from edc_reference.models import Reference
 
 
-class MaternalStatusHelper:
-
-    @property
-    def hiv_status(self):
-        return POS
-
-
 class Predicates(PredicateCollection):
 
     app_label = 'td_maternal'
     visit_model = f'{app_label}.maternalvisit'
 
-    def func_mother_pos(self, visit=None, **kwargs):
+    def func_mother_pos(self, visit=None, hiv_status=None, **kwargs):
         """Returns true if mother is hiv positive."""
-        maternal_status_helper = MaternalStatusHelper()
-        return maternal_status_helper.hiv_status == POS
+        return hiv_status == POS
 
-    def func_mother_neg(self, visit=None, **kwargs):
+    def func_mother_neg(self, visit=None, hiv_status=None, **kwargs):
         """Returns true if mother is hiv neg."""
-        maternal_status_helper = MaternalStatusHelper()
-        return maternal_status_helper.hiv_status == NEG
+        return hiv_status == NEG
 
-    def func_show_elisa_requisition_hiv_status_ind(self, visit=None, **kwargs):
+    def func_show_elisa_requisition_hiv_status_ind(self, visit=None,
+                                                   hiv_status=None, **kwargs):
         """return True if Mother's Rapid Test Result is Inditerminate"""
-        maternal_status_helper = MaternalStatusHelper()
-        return maternal_status_helper.hiv_status == IND
+        return hiv_status == IND
 
-    def func_mother_pos_vl(self, visit=None, **kwargs):
+    def func_mother_pos_vl(self, visit=None, hiv_status=None, **kwargs):
 
         visit_list = ['2000M', '2010M', '2020M', '2020M', '2060M']
-        return self.func_mother_pos(visit) and visit.visit_code in visit_list
+        return self.func_mother_pos(visit, hiv_status) and visit.visit_code in visit_list
 
     def func_show_postpartum_depression(self, visit=None, **kwargs):
         visit_list = ['2020M', '2060M', '2120M', '2180M', '2240M',
@@ -41,7 +32,8 @@ class Predicates(PredicateCollection):
         if visit.visit_code in visit_list:
             return Reference.objects.filter(
                 model=f'{self.app_label}.maternalpostpartumdep',
-                identifier=visit.subject_identifier).exists()
+                identifier=visit.subject_identifier,
+                report_datetime__lt=visit.report_datetime).exists()
 
     def func_show_ultrasound_form(self, visit=None, **kwargs):
         """Return true if ultrasound form has to be filled."""
@@ -54,26 +46,29 @@ class Predicates(PredicateCollection):
                 timepoint='1000M').exists()
         return False
 
-    def func_show_rapid_test_form(self, visit=None, **kwargs):
+    def func_show_rapid_test_form(self, visit=None,
+                                  hiv_status=None, **kwargs):
         subject_identifier = visit.subject_identifier
-        maternal_status_helper = MaternalStatusHelper()
-        if visit.visit_code == '2000M' and maternal_status_helper.hiv_status == NEG:
+#         maternal_status_helper = MaternalStatusHelper()
+        if visit.visit_code == '2000M' and hiv_status == NEG:
             prev_rapid_test = Reference.objects.filter(
                 model=f'{self.app_label}.rapidtestresult',
                 report_datetime__lt=visit.report_datetime,
-                identifier=subject_identifier).order_by('-report_datetime')
+                identifier=subject_identifier).order_by('-report_datetime').last()
 
             maternal_ultrasound = Reference.objects.filter(
                 model=f'{self.app_label}.maternalultrasoundinitial',
                 report_datetime__lt=visit.report_datetime,
-                identifier=subject_identifier).order_by('-report_datetime')
+                identifier=subject_identifier).order_by('-report_datetime').last()
+
+            print(maternal_ultrasound.__dict__)
 
             if prev_rapid_test and maternal_ultrasound:
                 return (maternal_ultrasound.edd_confirmed - prev_rapid_test.result_date).days < 56
             else:
                 return True
         else:
-            return maternal_status_helper.hiv_status in [UNK, NEG]
+            return hiv_status in [UNK, NEG]
 
     def func_show_srh_services_utilization(self, visit=None, **kwargs):
         """Returns True if participant was referred to srh in the last visit."""
@@ -83,10 +78,10 @@ class Predicates(PredicateCollection):
 
         previous_maternal_contr = Reference.objects.filter(
             model=f'{self.app_label}.maternalcontraception',
-            identifier=visit.subject_identifier).exclude(
-                timepoint=visit.visit_code).order_by('-report_datetime').first()
+            identifier=visit.subject_identifier,
+            report_datetime__lt=visit.report_datetime).order_by('-report_datetime').first()
 
-        if visit.visit_code in visit_list:
+        if previous_maternal_contr and visit.visit_code in visit_list:
             values = self.exists(
                 reference_name=f'{self.app_label}.maternalcontraception',
                 subject_identifier=visit.subject_identifier,
--- td_metadata_rules/tests/test_predicates.py
+++ td_metadata_rules/tests/test_predicates.py
@@ -1,14 +1,13 @@
-from datetime import datetime
-
-from arrow.arrow import Arrow
+# from arrow.arrow import Arrow
 from dateutil.relativedelta import relativedelta
 from django.test import TestCase, tag
 from edc_base.tests import SiteTestCaseMixin
-from edc_constants.constants import YES, NO
+from edc_base.utils import get_utcnow
+from edc_constants.constants import YES, NO, NEG, POS
 from edc_reference import LongitudinalRefset
 from edc_reference.tests import ReferenceTestHelper
 
-from ..predicates import Predicates, MaternalStatusHelper
+from ..predicates import Predicates
 
 
 @tag('pr')
@@ -33,115 +32,195 @@ class TestPredicates(SiteTestCaseMixin, TestCase):
             visit_model=self.visit_model,
             subject_identifier=self.subject_identifier)
 
-        report_datetime = Arrow.fromdatetime(
-            datetime(2018, 12, 17)).datetime
+        report_datetime = get_utcnow()
+#         Arrow.fromdatetime(
+#             datetime(2018, 12, 17)).datetime
         self.reference_helper.create_visit(
-            report_datetime=report_datetime - relativedelta(days=1), timepoint='1000M')
+            report_datetime=report_datetime, timepoint='1000M')
 
         self.reference_helper.create_visit(
-            report_datetime=report_datetime,
+            report_datetime=report_datetime + relativedelta(days=1),
             timepoint='1010M')
         self.reference_helper.create_visit(
             report_datetime=report_datetime + relativedelta(days=3),
             timepoint='1020M')
         self.reference_helper.create_visit(
-            report_datetime=report_datetime + relativedelta(days=3),
+            report_datetime=report_datetime + relativedelta(days=5),
             timepoint='2000M')
 
         self.reference_helper.create_visit(
-            report_datetime=report_datetime + relativedelta(days=3),
+            report_datetime=report_datetime + relativedelta(days=7),
             timepoint='2010M')
+
+        self.reference_helper.create_visit(
+            report_datetime=report_datetime + relativedelta(days=9),
+            timepoint='2020M')
 
     def test_func_elisa_required(self):
         pc = Predicates()
 
         self.assertFalse(
-            pc.func_show_elisa_requisition_hiv_status_ind(self.maternla_visits[0]))
+            pc.func_show_elisa_requisition_hiv_status_ind(self.maternal_visits[0]))
 
     def test_func_mother_pos_vl_required(self):
         pc = Predicates()
 
         self.assertTrue(
-            pc.func_mother_pos_vl(self.maternla_visits[3]))
+            pc.func_mother_pos_vl(self.maternal_visits[3], POS))
 
     def test_func_mother_pos_vl_not_required(self):
         pc = Predicates()
 
         self.assertFalse(
-            pc.func_mother_pos_vl(self.maternla_visits[1]))
+            pc.func_mother_pos_vl(self.maternal_visits[1], POS))
 
-    def test_ultrasound_form_1000_required(self):
+    def test_ultrasound_form_required(self):
         pc = Predicates()
 
         self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[0].report_datetime,
+            report_datetime=self.maternal_visits[0].report_datetime,
             reference_name=f'{self.app_label}.maternalultrasoundinitial',
-            visit_code=self.maternla_visits[0].visit_code)
+            visit_code=self.maternal_visits[0].visit_code)
         self.assertTrue(
-            pc.func_show_ultrasound_form(self.maternla_visits[0]))
+            pc.func_show_ultrasound_form(self.maternal_visits[0]))
 
-    def test_ultrasound_form_not_1000_required(self):
+    def test_ultrasound_form_not_required(self):
         pc = Predicates()
 
         self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[1].report_datetime,
+            report_datetime=self.maternal_visits[1].report_datetime,
             reference_name=f'{self.app_label}.maternalultrasoundinitial',
-            visit_code=self.maternla_visits[1].visit_code)
+            visit_code=self.maternal_visits[1].visit_code)
         self.assertTrue(
-            pc.func_show_ultrasound_form(self.maternla_visits[1]))
+            pc.func_show_ultrasound_form(self.maternal_visits[1]))
 
-    def test_ultrasound_form_not_1000_not_required(self):
+    def test_ultrasound_form_not_not_required(self):
         pc = Predicates()
 
         self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[0].report_datetime,
+            report_datetime=self.maternal_visits[0].report_datetime,
             reference_name=f'{self.app_label}.maternalultrasoundinitial',
-            visit_code=self.maternla_visits[0].visit_code)
+            visit_code=self.maternal_visits[0].visit_code)
 
         self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[1].report_datetime,
+            report_datetime=self.maternal_visits[1].report_datetime,
             reference_name=f'{self.app_label}.maternalultrasoundinitial',
-            visit_code=self.maternla_visits[1].visit_code)
+            visit_code=self.maternal_visits[1].visit_code)
         self.assertFalse(
-            pc.func_show_ultrasound_form(self.maternla_visits[1]))
+            pc.func_show_ultrasound_form(self.maternal_visits[1]))
+
+#     @tag('pr2')
+#     def test_rapid_test_form_required(self):
+#         pc = Predicates()
+#
+#         self.reference_helper.create_for_model(
+#             report_datetime=self.maternal_visits[2].report_datetime,
+#             reference_name=f'{self.app_label}.rapidtestresult',
+#             visit_code=self.maternal_visits[2].visit_code,
+#             result_date=self.maternal_visits[2].report_datetime.date())
+#
+#         self.reference_helper.create_for_model(
+#             report_datetime=self.maternal_visits[1].report_datetime,
+#             reference_name=f'{self.app_label}.maternalultrasoundinitial',
+#             visit_code=self.maternal_visits[1].visit_code,
+#             edd_confirmed=(self.maternal_visits[2].report_datetime + relativedelta(days=50)).date())
+#
+#
+#         self.assertTrue(
+#             pc.func_show_rapid_test_form(self.maternal_visits[3], NEG))
+#
+#     @tag('pr2')
+#     def test_rapid_test_form_not_required(self):
+#         pc = Predicates()
+#
+#         self.assertTrue(
+#             pc.func_show_rapid_test_form(self.maternal_visits[2], NEG))
+#
+#         self.reference_helper.create_for_model(
+#             report_datetime=self.maternal_visits[2].report_datetime,
+#             reference_name=f'{self.app_label}.rapidtestresult',
+#             visit_code=self.maternal_visits[2].visit_code,
+#             result_date=self.maternal_visits[2].report_datetime.date())
+#
+#         self.assertTrue(
+#             pc.func_show_rapid_test_form(self.maternal_visits[2], NEG))
+#
+#     @tag('pr2')
+#     def test_rapid_test_form_not_required_2(self):
+#         pc = Predicates()
+#
+#         self.assertTrue(
+#             pc.func_show_rapid_test_form(self.maternal_visits[2], NEG))
+#
+#         self.reference_helper.create_for_model(
+#             report_datetime=self.maternal_visits[2].report_datetime,
+#             reference_name=f'{self.app_label}.rapidtestresult',
+#             visit_code=self.maternal_visits[2].visit_code,
+#             result_date=self.maternal_visits[2].report_datetime.date())
+#
+#         self.assertTrue(
+#             pc.func_show_rapid_test_form(self.maternal_visits[2], NEG))
+#
+#     def test_srh_services_utilization_required(self):
+#         pc = Predicates()
+#
+#         self.reference_helper.create_for_model(
+#             report_datetime=self.maternal_visits[3].report_datetime,
+#             reference_name=f'{self.app_label}.maternalcontraception',
+#             visit_code=self.maternal_visits[3].visit_code,
+#             srh_referral=YES)
+#
+#         self.reference_helper.create_for_model(
+#             report_datetime=self.maternal_visits[4].report_datetime,
+#             reference_name=f'{self.app_label}.maternalcontraception',
+#             visit_code=self.maternal_visits[4].visit_code)
+#         self.assertTrue(
+#             pc.func_show_srh_services_utilization(self.maternal_visits[4]))
+#         pc = Predicates()
+
+    def test_srh_services_utilization_not_required(self):
+        pc = Predicates()
+
+        self.reference_helper.create_for_model(
+            report_datetime=self.maternal_visits[3].report_datetime,
+            reference_name=f'{self.app_label}.maternalcontraception',
+            visit_code=self.maternal_visits[3].visit_code,
+            srh_referral=NO)
+
+        self.reference_helper.create_for_model(
+            report_datetime=self.maternal_visits[4].report_datetime,
+            reference_name=f'{self.app_label}.maternalcontraception',
+            visit_code=self.maternal_visits[4].visit_code)
+        self.assertFalse(
+            pc.func_show_srh_services_utilization(self.maternal_visits[4]))
+
+    def test_srh_services_utilization_not_required_2(self):
+        pc = Predicates()
+
+        self.reference_helper.create_for_model(
+            report_datetime=self.maternal_visits[3].report_datetime,
+            reference_name=f'{self.app_label}.maternalcontraception',
+            visit_code=self.maternal_visits[3].visit_code,
+            srh_referral=NO)
+
+        self.reference_helper.create_for_model(
+            report_datetime=self.maternal_visits[5].report_datetime,
+            reference_name=f'{self.app_label}.maternalcontraception',
+            visit_code=self.maternal_visits[5].visit_code,
+            srh_referral=YES)
+
+        self.reference_helper.create_for_model(
+            report_datetime=self.maternal_visits[4].report_datetime,
+            reference_name=f'{self.app_label}.maternalcontraception',
+            visit_code=self.maternal_visits[4].visit_code)
+        self.assertFalse(
+            pc.func_show_srh_services_utilization(self.maternal_visits[4]))
 
     @property
-    def maternla_visits(self):
+    def maternal_visits(self):
         return LongitudinalRefset(
             subject_identifier=self.subject_identifier,
             visit_model=self.visit_model,
             name=self.visit_model,
             reference_model_cls=self.reference_model
         ).order_by('report_datetime')
-
-    def test_srh_services_utilization_required(self):
-        pc = Predicates()
-
-        self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[3].report_datetime,
-            reference_name=f'{self.app_label}.maternalcontraception',
-            visit_code=self.maternla_visits[3].visit_code,
-            srh_referral=YES)
-
-        self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[4].report_datetime,
-            reference_name=f'{self.app_label}.maternalcontraception',
-            visit_code=self.maternla_visits[4].visit_code)
-        self.assertTrue(
-            pc.func_show_srh_services_utilization(self.maternla_visits[4]))
-
-    def test_srh_services_utilization_not_required(self):
-        pc = Predicates()
-
-        self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[3].report_datetime,
-            reference_name=f'{self.app_label}.maternalcontraception',
-            visit_code=self.maternla_visits[3].visit_code,
-            srh_referral=NO)
-
-        self.reference_helper.create_for_model(
-            report_datetime=self.maternla_visits[4].report_datetime,
-            reference_name=f'{self.app_label}.maternalcontraception',
-            visit_code=self.maternla_visits[4].visit_code)
-        self.assertFalse(
-            pc.func_show_srh_services_utilization(self.maternla_visits[4]))
