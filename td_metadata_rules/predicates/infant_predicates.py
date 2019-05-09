d8f7fbd0636ea09a3cffb1294f735188cf0af0c2
Leslie <leslie.kep@gmail.com>
2019-05-09 12:45:11 +0200

    update karabo rule queries

--- td_metadata_rules/predicates/infant_predicates.py
+++ td_metadata_rules/predicates/infant_predicates.py
@@ -13,8 +13,8 @@ class InfantPredicates(PredicateCollection):
     app_label = 'td_infant'
     visit_model = f'{app_label}.infantvisit'
     karabo_tb_model = f'{app_label}.karabotuberculosishistory'
-    karabo_consent_model = f'{app_label}.karabosubjectconsent'
-    karabo_screening_model = f'{app_label}.karabosubjectscreening'
+    karabo_consent_model = f'td_maternal.karabosubjectconsent'
+    karabo_screening_model = f'td_maternal.karabosubjectscreening'
     registered_subject_model = 'edc_registration.registeredsubject'
     maternal_visit_model = 'td_maternal.maternalvisit'
 
@@ -139,18 +139,26 @@ class InfantPredicates(PredicateCollection):
         return visit.appointment.timepoint < 180 and value[0] == YES
 
     def func_show_karabo_tb_history(self, visit, **kwargs):
+
         if not self.show_karabo_offstudy(visit=visit):
-            subject_identifier = visit.appointment.subject_identifier
+
+            try:
+                maternal_subject_id = self.registered_subject_model_cls.objects.get(
+                    subject_identifier=visit.appointment.subject_identifier).relative_identifier
+            except self.registered_subject_model_cls.DoesNotExist:
+                raise ValidationError(
+                    'Maternal registered subject not found for '
+                    f'infant id: {visit.appointment.subject_identifier}')
             try:
                 karabo_screening = self.karabo_screening_model_cls.objects.get(
-                    subject_identifier=subject_identifier)
+                    subject_identifier=maternal_subject_id)
             except self.karabo_screening_model_cls.DoesNotExist:
                 return False
             else:
                 if karabo_screening.is_eligible:
                     try:
                         self.karabo_consent_model_cls.objects.get(
-                            subject_identifier=subject_identifier)
+                            subject_identifier=maternal_subject_id)
                         return True
                     except self.karabo_consent_model_cls.DoesNotExist:
                         raise ValidationError(
